1bd0bf847a4b962b9c58c3f6558007543e187e84
russell <russell.g5628@gmail.com>
2022-03-21 20:16:21 +0900

    Update Polishing

--- coach/flutter_service_worker.js
+++ coach/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "556b1839934f3be0da93dd22cfdfb0e1",
-"index.html": "414144a35b899408b172b41b52ad8b86",
-"/": "414144a35b899408b172b41b52ad8b86",
-"main.dart.js": "a22ce7778c66f466eadc529630418cbf",
+"index.html": "97f08b0322d185c5270cb8507580566c",
+"/": "97f08b0322d185c5270cb8507580566c",
+"main.dart.js": "d988a83bcfb6667c60ae717ded0ec16a",
 "favicon.png": "15c8ead158201651c5f79e36a7e26810",
 "icons/Icon-192.png": "0ea6bd37110923bfcad2147ad2ca5621",
 "icons/Icon-maskable-192.png": "caa7d47048c84003fc5ed8702e737bcd",
